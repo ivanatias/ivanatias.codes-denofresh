217148dd3032b63014191813453069c452247944
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-02 17:13:49 -0400

    add 404 page to generator

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,13 +3,15 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from './deno.json' assert { type: 'json' }
-import * as $0 from './routes/_app.tsx'
-import * as $1 from './routes/index.tsx'
+import * as $0 from './routes/_404.tsx'
+import * as $1 from './routes/_app.tsx'
+import * as $2 from './routes/index.tsx'
 
 const manifest = {
 	routes: {
-		'./routes/_app.tsx': $0,
-		'./routes/index.tsx': $1,
+		'./routes/_404.tsx': $0,
+		'./routes/_app.tsx': $1,
+		'./routes/index.tsx': $2,
 	},
 	islands: {},
 	baseUrl: import.meta.url,
