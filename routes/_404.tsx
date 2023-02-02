db7ad7455970124a11a27d85ccc4a85a2314cb24
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-02 17:12:49 -0400

    feat: add not found page

---
+++ routes/_404.tsx
@@ -0,0 +1,20 @@
+import { Head } from '$fresh/runtime.ts'
+import { UnknownPageProps } from '$fresh/server.ts'
+
+const NotFoundPage = ({ url: { pathname } }: UnknownPageProps) => (
+	<>
+		<Head>
+			<title>Ivan Atias - 404 Not Found</title>
+		</Head>
+		<div class='grid h-screen gap-3 text(black dark:white) place-content-center'>
+			<h1 class='text(4xl 2xl:5xl) font-bold'>
+				The page: {pathname} was not found
+			</h1>
+			<a href='/' class='text(base center underline 2xl:lg) font-semibold'>
+				Go back home
+			</a>
+		</div>
+	</>
+)
+
+export default NotFoundPage
