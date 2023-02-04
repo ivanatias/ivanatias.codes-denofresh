cb472e30c640ff14a9695a1ae914dc57b687a5c8
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-03 21:18:48 -0400

    feat(constants): add skills data

---
+++ constants/skills.ts
@@ -0,0 +1,64 @@
+const skills = [
+  {
+    skillImage: '/images/reactjs.svg',
+    skillText: 'React.js',
+  },
+  {
+    skillImage: '/images/nextjs.svg',
+    skillText: 'Next.js',
+  },
+  {
+    skillImage: '/images/gatsbyjs.svg',
+    skillText: 'Gatsby.js',
+  },
+  {
+    skillImage: '/images/javascript.svg',
+    skillText: 'JavaScript',
+  },
+  {
+    skillImage: '/images/typescript.svg',
+    skillText: 'TypeScript',
+  },
+  {
+    skillImage: '/images/nodejs.svg',
+    skillText: 'Node.js',
+  },
+  {
+    skillImage: '/images/mongodb.svg',
+    skillText: 'MongoDB',
+  },
+  {
+    skillImage: '/images/css.svg',
+    skillText: 'CSS',
+  },
+  {
+    skillImage: '/images/html.svg',
+    skillText: 'HTML',
+  },
+  {
+    skillImage: '/images/sass.svg',
+    skillText: 'Sass',
+  },
+  {
+    skillImage: '/images/tailwindcss.svg',
+    skillText: 'TailwindCSS',
+  },
+  {
+    skillImage: '/images/materialui.svg',
+    skillText: 'Material UI',
+  },
+  {
+    skillImage: '/images/bootstrap.svg',
+    skillText: 'Bootstrap',
+  },
+  {
+    skillImage: '/images/git.svg',
+    skillText: 'Git',
+  },
+  {
+    skillImage: '/images/figma.svg',
+    skillText: 'Figma',
+  },
+]
+
+export { skills }
