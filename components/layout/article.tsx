e8deea8d82d29f7852d24be07f671526be792da6
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-05 18:42:23 -0400

    refactor(article): provide support for custom styling

--- components/layout/article.tsx
+++ components/layout/article.tsx
@@ -2,10 +2,13 @@ import { ComponentChildren } from 'preact'
 
 interface Props {
   children: ComponentChildren
+  className?: string
 }
 
-const Article = ({ children }: Props) => (
-  <article class='flex(& col) w-full gap-4'>
+const defaultStyles = 'flex(& col) w-full gap-4'
+
+const Article = ({ children, className }: Props) => (
+  <article class={className ?? defaultStyles}>
     {children}
   </article>
 )
