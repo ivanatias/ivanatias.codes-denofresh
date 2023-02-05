afcd61e30e01a308a2d232a41e32d3786fd81e51
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-05 18:41:36 -0400

    feat: add work card component

---
+++ components/work-card.tsx
@@ -0,0 +1,36 @@
+import Link from 'components/link.tsx'
+import Title from 'components/layout/title.tsx'
+import Paragraph from 'components/layout/paragraph.tsx'
+import { getImageDimensions } from 'sanity/asset-utils'
+import { Work } from 'models/works.d.ts'
+
+type Props = Omit<Work, '_id'>
+
+const WorkCard = ({ thumbNail, title, overview, slug }: Props) => {
+  const { width, height } = getImageDimensions(thumbNail.asset)
+
+  return (
+    <Link
+      href={`/works/${slug.current}`}
+      className='flex(& col) items-center justify-center lg:transition-transform lg:duration-300 lg:hover:scale-110'
+    >
+      <img
+        src={thumbNail.asset.url}
+        alt={title}
+        width={width}
+        height={height}
+        class='w-full h-auto rounded-lg'
+        decoding='async'
+        loading='lazy'
+      />
+      <div class='flex(& col ) items-center justify-center w-full gap-1 mt-3'>
+        <Title titleTag='h3' titleClass='small'>
+          {title}
+        </Title>
+        <Paragraph pClass='small'>{overview}</Paragraph>
+      </div>
+    </Link>
+  )
+}
+
+export default WorkCard
