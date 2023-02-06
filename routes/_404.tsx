63ce44297896c7e428f7aad25026ea334ba4e350
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 00:52:41 -0400

    styles(404): add padding and center text

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -8,8 +8,8 @@ const NotFoundPage = ({ url: { pathname } }: UnknownPageProps) => (
       <title>Ivan Atias - 404 Not Found</title>
     </Head>
     <Navbar />
-    <div class='grid h-screen gap-3 text(black dark:white) place-content-center'>
-      <h1 class='text(4xl 2xl:5xl) font-bold'>
+    <div class='p-4 grid h-screen gap-3 text(black dark:white) place-content-center'>
+      <h1 class='text(center 4xl 2xl:5xl) font-bold'>
         The page: {pathname} was not found
       </h1>
       <a href='/' class='text(base center underline 2xl:lg) font-semibold'>
