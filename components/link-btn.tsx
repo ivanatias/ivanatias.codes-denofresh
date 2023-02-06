68931d0aa53a3584b72b6c8663bc7c4b6c32a3a8
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 00:21:39 -0400

    refactor(link-btn): destructure children and className props

--- components/link-btn.tsx
+++ components/link-btn.tsx
@@ -3,15 +3,19 @@ import Link, { Props } from 'components/link.tsx'
 const defaultStyle =
   'w-full max-w([150px] 2xl:[200px]) py-2 px-5 bg(indigo-700 dark:indigo-600) text(center gray-100 sm 2xl:lg) font-semibold rounded-md hover:bg-indigo-800 dark:hover:bg-indigo-700 transition duration-150'
 
-const LinkButton = (props: Props) => (
-  <div class='flex items-center justify-center mt-4'>
-    <Link
-      className={props.className ?? defaultStyle}
-      {...props}
-    >
-      {props.children}
-    </Link>
-  </div>
-)
+const LinkButton = (props: Props) => {
+  const { children, className, ...restOfProps } = props
+
+  return (
+    <div class='flex items-center justify-center mt-4'>
+      <Link
+        className={className ?? defaultStyle}
+        {...restOfProps}
+      >
+        {children}
+      </Link>
+    </div>
+  )
+}
 
 export default LinkButton
