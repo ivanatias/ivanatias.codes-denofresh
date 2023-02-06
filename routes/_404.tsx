0882125190ef8c7a2109646f2ffea375257a5869
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 01:14:01 -0400

    refactor(404): remove pathname from message and modify meta title

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -3,15 +3,15 @@ import { UnknownPageProps } from '$fresh/server.ts'
 import Navbar from 'islands/navbar.tsx'
 import Link from 'components/link.tsx'
 
-const NotFoundPage = ({ url: { pathname } }: UnknownPageProps) => (
+const NotFoundPage = (_props: UnknownPageProps) => (
   <>
     <Head>
-      <title>Ivan Atias - 404 Not Found</title>
+      <title>404 Not Found</title>
     </Head>
     <Navbar />
     <div class='p-4 grid h-screen gap-3 text(black dark:white) place-content-center'>
       <h1 class='text(center 4xl 2xl:5xl) font-bold'>
-        The page: {pathname} was not found
+        The page was not found
       </h1>
       <Link
         href='/'
