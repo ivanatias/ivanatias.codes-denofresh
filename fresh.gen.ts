f4ae2f792b71b39f092942c44774913ad4d28321
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 01:00:40 -0400

    feat: add internal server error page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,19 +4,21 @@
 
 import config from './deno.json' assert { type: 'json' }
 import * as $0 from './routes/_404.tsx'
-import * as $1 from './routes/_app.tsx'
-import * as $2 from './routes/blog/index.tsx'
-import * as $3 from './routes/index.tsx'
-import * as $4 from './routes/works/index.tsx'
+import * as $1 from './routes/_500.tsx'
+import * as $2 from './routes/_app.tsx'
+import * as $3 from './routes/blog/index.tsx'
+import * as $4 from './routes/index.tsx'
+import * as $5 from './routes/works/index.tsx'
 import * as $$0 from './islands/navbar.tsx'
 
 const manifest = {
   routes: {
     './routes/_404.tsx': $0,
-    './routes/_app.tsx': $1,
-    './routes/blog/index.tsx': $2,
-    './routes/index.tsx': $3,
-    './routes/works/index.tsx': $4,
+    './routes/_500.tsx': $1,
+    './routes/_app.tsx': $2,
+    './routes/blog/index.tsx': $3,
+    './routes/index.tsx': $4,
+    './routes/works/index.tsx': $5,
   },
   islands: {
     './islands/navbar.tsx': $$0,
---
+++ routes/_500.tsx
@@ -0,0 +1,26 @@
+import { Head } from '$fresh/runtime.ts'
+import { ErrorPageProps } from '$fresh/server.ts'
+import Navbar from 'islands/navbar.tsx'
+import Link from 'components/link.tsx'
+
+const Error500Page = ({ error }: ErrorPageProps) => (
+  <>
+    <Head>
+      <title>500 Internal server error</title>
+    </Head>
+    <Navbar />
+    <div class='p-4 grid h-screen gap-3 text(black dark:white) place-content-center'>
+      <h1 class='text(center 4xl 2xl:5xl) font-bold'>
+        500 internal error: {(error as Error).message}
+      </h1>
+      <Link
+        href='/'
+        className='text(base center underline 2xl:lg) font-semibold'
+      >
+        Try again
+      </Link>
+    </div>
+  </>
+)
+
+export default Error500Page
