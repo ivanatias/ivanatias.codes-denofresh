fd57cb7719fc1dced423e853919db68e938f594e
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 00:13:21 -0400

    feat(links): add source code link and isExternal/icon attributes

--- constants/links.ts
+++ constants/links.ts
@@ -2,14 +2,23 @@ const links = [
   {
     path: '/',
     label: 'Home',
+    isExternal: false,
   },
   {
     path: '/works',
     label: 'Works',
+    isExternal: false,
   },
   {
     path: '/blog',
     label: 'Blog',
+    isExternal: false,
+  },
+  {
+    path: 'https://github.com/ivanatias/ivanatias.codes-denofresh',
+    label: 'Source code',
+    isExternal: true,
+    icon: 'icon-github',
   },
 ]
 
