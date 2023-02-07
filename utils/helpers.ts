cafe7696ef53231c80f1150e7ef7191f69e2733f
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 22:13:38 -0400

    feat: add format reading time utility function

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,9 @@
 const formatDate = (date: string) => date.substring(0, 10)
 
-export { formatDate }
+const formatReadingTime = (readingMinutes: number) => {
+  return readingMinutes > 1
+    ? `${readingMinutes} minutes read`
+    : `${readingMinutes} minute read`
+}
+
+export { formatDate, formatReadingTime }
