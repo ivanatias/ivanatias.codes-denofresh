68b2e4893425be031e81d2425f79ec314a379b75
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-06 22:16:36 -0400

    feat: add blog post page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,10 +6,11 @@ import config from './deno.json' assert { type: 'json' }
 import * as $0 from './routes/_404.tsx'
 import * as $1 from './routes/_500.tsx'
 import * as $2 from './routes/_app.tsx'
-import * as $3 from './routes/blog/index.tsx'
-import * as $4 from './routes/index.tsx'
-import * as $5 from './routes/works/[slug].tsx'
-import * as $6 from './routes/works/index.tsx'
+import * as $3 from './routes/blog/[slug].tsx'
+import * as $4 from './routes/blog/index.tsx'
+import * as $5 from './routes/index.tsx'
+import * as $6 from './routes/works/[slug].tsx'
+import * as $7 from './routes/works/index.tsx'
 import * as $$0 from './islands/navbar.tsx'
 
 const manifest = {
@@ -17,10 +18,11 @@ const manifest = {
     './routes/_404.tsx': $0,
     './routes/_500.tsx': $1,
     './routes/_app.tsx': $2,
-    './routes/blog/index.tsx': $3,
-    './routes/index.tsx': $4,
-    './routes/works/[slug].tsx': $5,
-    './routes/works/index.tsx': $6,
+    './routes/blog/[slug].tsx': $3,
+    './routes/blog/index.tsx': $4,
+    './routes/index.tsx': $5,
+    './routes/works/[slug].tsx': $6,
+    './routes/works/index.tsx': $7,
   },
   islands: {
     './islands/navbar.tsx': $$0,
---
+++ routes/blog/[slug].tsx
@@ -0,0 +1,105 @@
+import { Head } from '$fresh/runtime.ts'
+import { Handlers, PageProps } from '$fresh/server.ts'
+import Layout from 'components/layout/index.tsx'
+import MetaData from 'components/meta-data.tsx'
+import Section from 'components/layout/section.tsx'
+import Article from 'components/layout/article.tsx'
+import Title from 'components/layout/title.tsx'
+import CustomPortableText from 'components/sanity-block-contents/portable-text/custom-portabletext.tsx'
+import { client } from 'lib/sanity-client.ts'
+import { getBlogPostQuery, getBlogPostReadingTimeQuery } from 'utils/queries.ts'
+import { ArticleReadingTime, BlogArticle } from 'models/article.d.ts'
+import { formatDate, formatReadingTime } from 'utils/helpers.ts'
+
+interface Props extends BlogArticle {
+  readingTime: ArticleReadingTime
+}
+
+export const handler: Handlers<Props> = {
+  async GET(_req, ctx) {
+    const { slug } = ctx.params
+    const blogPostQuery = getBlogPostQuery(slug)
+    const readingTimeQuery = getBlogPostReadingTimeQuery(slug)
+
+    const [blogPostData, readingTime] = await Promise.all([
+      client.fetch<BlogArticle>(blogPostQuery),
+      client.fetch<ArticleReadingTime>(readingTimeQuery),
+    ])
+
+    return blogPostData.currentPost === null
+      ? ctx.renderNotFound()
+      : ctx.render({ ...blogPostData, readingTime })
+  },
+}
+
+const BlogPost = ({ data }: PageProps<Props>) => {
+  const {
+    currentPost,
+    previousPost,
+    nextPost,
+    readingTime: { estimatedReadingTime },
+  } = data
+
+  const {
+    articleTitle,
+    articleBody,
+    coverImage,
+    publishDate,
+    slug,
+    socialShareImage,
+    excerpt,
+  } = currentPost
+
+  const { altText, image: { asset: { url: coverImageUrl } } } = coverImage
+  const { asset: { url: socialImageUrl } } = socialShareImage
+
+  const date = formatDate(publishDate)
+  const readingTime = formatReadingTime(estimatedReadingTime)
+
+  return (
+    <>
+      <Head>
+        <link rel='stylesheet' href='/styles/prism-theme.css' />
+      </Head>
+      <MetaData
+        title={articleTitle}
+        canonicalUrlPath={`/blog/${slug.current}`}
+        description={excerpt}
+        socialCardImage={socialImageUrl}
+        contentType='article'
+      />
+      <Layout>
+        <Section>
+          <Article>
+            <div class='flex(& col) w-full gap-3'>
+              <img
+                src={coverImageUrl}
+                alt={altText}
+                width='56'
+                height='56'
+                class='flex-shrink-0 w-14 h-14'
+              />
+              <Title titleTag='h2' titleClass='lg'>
+                {articleTitle}
+              </Title>
+            </div>
+            <div class='flex items-center gap-3'>
+              <time
+                dateTime={publishDate}
+                class='text(xs 2xl:sm black dark:gray-400)'
+              >
+                {date}
+              </time>
+              <span class='text(xs 2xl:sm black dark:gray-400) underline'>
+                {readingTime}
+              </span>
+            </div>
+            <CustomPortableText articleBody={articleBody} />
+          </Article>
+        </Section>
+      </Layout>
+    </>
+  )
+}
+
+export default BlogPost
