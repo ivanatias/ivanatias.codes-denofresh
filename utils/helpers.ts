e3b9460bd87fd6bb58ee902202a49ddd8a4d8c17
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-07 02:00:55 -0400

    feat: add utility functions for share icons display

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -6,4 +6,21 @@ const formatReadingTime = (readingMinutes: number) => {
     : `${readingMinutes} minute read`
 }
 
-export { formatDate, formatReadingTime }
+const calculateIconTransition = (position: number, multiplicator: number) => {
+  return `top 0.2s ${position * multiplicator}ms, left 0.2s ${
+    position * multiplicator
+  }ms, opacity 0.2s ${position * multiplicator}ms`
+}
+
+const calculateIconLeftPosition = (position: number, multiplicator: number) => {
+  return `calc(${
+    (-1) ** position * Math.ceil(position / 2) * multiplicator
+  }px + 6px)`
+}
+
+export {
+  calculateIconLeftPosition,
+  calculateIconTransition,
+  formatDate,
+  formatReadingTime,
+}
