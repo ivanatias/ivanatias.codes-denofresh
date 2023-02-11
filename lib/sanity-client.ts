7ee0b25cb6678715c69463d1fdc97bb396cdcac6
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-11 16:34:25 -0400

    chore(sanity-client): update api version and add token

--- lib/sanity-client.ts
+++ lib/sanity-client.ts
@@ -4,7 +4,8 @@ import imageUrlBuilder from 'sanity/image-url'
 const client = sanityClient({
   projectId: 'uaj0umn5',
   dataset: 'production',
-  apiVersion: '2022-11-09',
+  apiVersion: '2023-02-11',
+  token: Deno.env.get('SANITY_TOKEN'),
   useCdn: true,
 })
 
