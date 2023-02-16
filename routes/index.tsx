ecde70498f8452e608b3b415db17a98f57df27e8
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-15 20:27:37 -0400

    refactor(index): implicitly return jsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,65 +20,66 @@ export const handler: Handlers<Biography[]> = {
   },
 }
 
-export default function Home({ data }: PageProps<Biography[]>) {
-  return (
-    <>
-      <MetaData />
-      <Layout>
-        <Section>
-          <Article>
-            <Title titleTag='h2' titleClass='lg'>
-              Work
-            </Title>
-            <Paragraph>
-              I&apos;m a Front-end Engineer and UI Designer who enjoys a lot
-              building and designing good looking and functional websites and
-              apps. These days, most of my work revolves around technologies
-              such as React.js, Next.js, Gatsby.js and recently, React Native.
-              I&apos;m also comfortable using CSS technologies such as Sass,
-              Bootstrap, Material UI and TailwindCSS. For my designs, Figma is
-              my predilect design and prototyping tool.
-            </Paragraph>
-            <LinkButton href='/works'>
-              My portfolio
-            </LinkButton>
-          </Article>
-          <Article>
-            <Title titleTag='h2' titleClass='lg'>
-              Skills
-            </Title>
-            <Paragraph>
-              These are some of the technologies and tools I&apos;m comfortable
-              with:
-            </Paragraph>
-            <Skills />
-          </Article>
-          <Article>
-            <Title titleTag='h2' titleClass='lg'>
-              Biography
-            </Title>
-            {data.map(({ _id, ...item }) => (
-              <BiographyItem key={_id} {...item} />
-            ))}
-          </Article>
-          <Article>
-            <Title titleTag='h2' titleClass='lg'>
-              Personal Blog
-            </Title>
-            <Paragraph>
-              A space for sharing and expanding knowledge. I write about a wide
-              variety of topics related to technology in this section,
-              expressing my personal opinions on subjects such as web
-              development, web performance, user interface design, and other
-              miscellaneous ones. The aim of this blog is to document my journey
-              as a Front-end Engineer and UI Designer one article at a time.
-            </Paragraph>
-            <LinkButton href='/blog'>
-              See blog
-            </LinkButton>
-          </Article>
-        </Section>
-      </Layout>
-    </>
-  )
-}
+const Home = ({ data }: PageProps<Biography[]>) => (
+  <>
+    <MetaData />
+    <Layout>
+      <Section>
+        <Article>
+          <Title titleTag='h2' titleClass='lg'>
+            Work
+          </Title>
+          <Paragraph>
+            I&apos;m a Front-end Engineer and UI Designer who enjoys a lot
+            building and designing good looking and functional websites and
+            apps. These days, most of my work revolves around technologies such
+            as React.js, Next.js, Gatsby.js and recently, React Native. I&apos;m
+            also comfortable using CSS technologies such as Sass, Bootstrap,
+            Material UI and TailwindCSS. For my designs, Figma is my predilect
+            design and prototyping tool.
+          </Paragraph>
+          <LinkButton href='/works'>
+            My portfolio
+          </LinkButton>
+        </Article>
+        <Article>
+          <Title titleTag='h2' titleClass='lg'>
+            Skills
+          </Title>
+          <Paragraph>
+            These are some of the technologies and tools I&apos;m comfortable
+            with:
+          </Paragraph>
+          <Skills />
+        </Article>
+        <Article>
+          <Title titleTag='h2' titleClass='lg'>
+            Biography
+          </Title>
+          {data.map(({ _id, ...item }) => <BiographyItem
+            key={_id}
+            {...item}
+          />)}
+        </Article>
+        <Article>
+          <Title titleTag='h2' titleClass='lg'>
+            Personal Blog
+          </Title>
+          <Paragraph>
+            A space for sharing and expanding knowledge. I write about a wide
+            variety of topics related to technology in this section, expressing
+            my personal opinions on subjects such as web development, web
+            performance, user interface design, and other miscellaneous ones.
+            The aim of this blog is to document my journey as a Front-end
+            Engineer and UI Designer one article at a time.
+          </Paragraph>
+          <LinkButton href='/blog'>
+            See blog
+          </LinkButton>
+        </Article>
+      </Section>
+    </Layout>
+  </>
+)
+
+export default Home
