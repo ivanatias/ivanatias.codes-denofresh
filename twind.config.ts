a956350ff7de389cb95e2a85312e25b2d791f9b4
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-17 09:36:11 -0400

    styles: add blur backdrop

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,12 @@
 import { Options } from '$fresh/plugins/twind.ts'
 
 export default {
+  plugins: {
+    'blur-backdrop': {
+      backdropFilter: 'blur(6px)',
+      backgroundColor: 'transparent',
+    },
+  },
   selfURL: import.meta.url,
   darkMode: 'class',
 } as Options
