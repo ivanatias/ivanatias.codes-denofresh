83fceaac86ad91ade2fcb00c178fba8e20133963
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-17 09:57:22 -0400

    styles: add rgba backgrounds for better backdrop effect

--- twind.config.ts
+++ twind.config.ts
@@ -3,8 +3,15 @@ import { Options } from '$fresh/plugins/twind.ts'
 export default {
   plugins: {
     'blur-backdrop': {
-      backdropFilter: 'blur(6px)',
-      backgroundColor: 'transparent',
+      backdropFilter: 'blur(8px)',
+    },
+  },
+  theme: {
+    extend: {
+      colors: {
+        'white-rgba': 'rgba(255, 255, 255, 0.35)',
+        'dark-rgba': 'rgba(13, 17, 23, 0.15)',
+      },
     },
   },
   selfURL: import.meta.url,
