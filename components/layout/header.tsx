9b2e393d970a3af277cd18cf0235a35487044a98
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-13 15:35:12 -0400

    refactor(header): restyle elements

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,26 +3,34 @@ import Paragraph from 'components/layout/paragraph.tsx'
 import Social from 'components/social.tsx'
 
 const Header = () => (
-  <header class='flex(& col) items-center gap-5 pb-12 pt-32'>
-    <img
-      src='/profile-pic.webp'
-      class='rounded-full ring(& 2 gray-900 dark:gray-100)'
-      width='150'
-      height='150'
-      alt='Ivan Atias, Front-end Engineer and UI Designer'
-      decoding='async'
-    />
-    <div class='flex(& col) items-center gap-2'>
+  <header class='flex(& col) pt-40 pb-24 gap-12 relative'>
+    <div class='flex(& col) gap-4'>
+      <div class='flex(& col) gap-1'>
+        <span class='text(indigo-700 dark:indigo-400 2xl md:3xl lg:4xl) font-semibold'>
+          Code.
+        </span>
+        <span class='text(green-700 dark:green-400 2xl md:3xl lg:4xl) font-semibold pl-4'>
+          Design.
+        </span>
+        <span class='text(pink-700 dark:pink-400 2xl md:3xl lg:4xl) font-semibold pl-8'>
+          Create.
+        </span>
+      </div>
+      <Paragraph pClass='small'>
+        Aesthetic design meets functionality, performance, and scalability.
+      </Paragraph>
+    </div>
+    <div class='relative flex(& col) gap-2 items-center max-w-xs mx-auto'>
       <Title>
         Ivan Atias
       </Title>
-      <Paragraph pClass='small' semibold centered>
-        <span class='bg-gradient-to-b text-transparent bg-clip-text from-indigo-900 to-pink-900 dark:from-indigo-200 dark:to-pink-200'>
-          Front-end Engineer and UI Designer based in Venezuela
-        </span>
+      <Paragraph semibold centered>
+        Front-end Engineer and UI Designer
       </Paragraph>
+      <Social />
+      <div class='absolute bottom-[10%] left-0 rounded-full w-[70px] h-[110px] bg-gradient-to-l from(& pink-800 dark:pink-400) to(& indigo-800 dark:indigo-500) blur-filter' />
+      <div class='absolute -top-[72%] right-[12%] rounded-full w-[80px] h-[124px] bg(& green-600 dark:green-300) blur-filter' />
     </div>
-    <Social />
   </header>
 )
 
