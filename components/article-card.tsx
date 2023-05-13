835f3a7bf28eca00d796feadb96dceaf0dcb88f9
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-13 15:32:59 -0400

    styles(article-card): min-height and adjust image

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -3,7 +3,7 @@ import Title from 'components/layout/title.tsx'
 import Paragraph from 'components/layout/paragraph.tsx'
 import Article from 'components/layout/article.tsx'
 import type { Blog } from 'models/blogs.d.ts'
-import { formatDate } from 'utils/helpers.ts'
+import { formatDate, truncateText } from 'utils/helpers.ts'
 
 type Props = Omit<Blog, '_id'>
 
@@ -12,19 +12,20 @@ const ArticleCard = (
 ) => {
   const { altText, image: { asset: { url } } } = coverImage
   const date = formatDate(publishDate)
+  const truncatedExcerpt = truncateText(excerpt, 120)
 
   return (
     <Link
       href={`/blog/${slug.current}`}
       className=''
     >
-      <Article className='flex w-full gap-5 p-4 transition duration-300 border(& transparent) hover:border-indigo-800 dark:hover:border-indigo-400 rounded-md'>
+      <Article className='flex(& col) sm:min-h-[300px] w-full gap-5 p-4 transition duration-300 border(& dashed indigo-800 dark:indigo-400) rounded-md md:transition-transform md:duration-300 md:hover:scale-105'>
         <img
           src={url}
           alt={altText}
-          width='48'
-          height='48'
-          class='flex-shrink-0 w-12 h-12'
+          width='64'
+          height='64'
+          class='flex-shrink-0 w-16 h-16'
           decoding='async'
           loading='lazy'
         />
@@ -32,7 +33,7 @@ const ArticleCard = (
           <Title titleTag='h3' titleClass='small'>
             {articleTitle}
           </Title>
-          <Paragraph pClass='small'>{excerpt}</Paragraph>
+          <Paragraph pClass='small'>{truncatedExcerpt}</Paragraph>
           <time
             dateTime={publishDate}
             class='text(pink-800 dark:pink-400 xs 2xl:sm) font-semibold'
