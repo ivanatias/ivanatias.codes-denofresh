93caf48368fa4aa9317144723e0ce9920e7c2d02
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-21 22:34:57 -0400

    refactor(menu): separate menu lines into component

--- components/menu.tsx
+++ components/menu.tsx
@@ -7,29 +7,11 @@ interface MenuButtonProps {
   isActive: boolean
 }
 
-const MenuButton = ({ toggleMenu, isActive }: MenuButtonProps) => (
-  <button
-    onClick={toggleMenu}
-    class='flex(& col) justify-between items-center w-7 h-[22px] md:hidden focus:outline-none'
-    aria-expanded={isActive}
-    aria-label={isActive ? 'Close nav menu' : 'Open nav menu'}
-  >
-    <span
-      class={`${sharedMenuClasses} ${isActive && 'rotate-45'}`}
-      aria-hidden={true}
-    />
-    <span
-      class={`${sharedMenuClasses} ${isActive && 'opacity-0'}`}
-      aria-hidden={true}
-    />
-    <span
-      class={`${sharedMenuClasses} ${isActive && '-rotate-45'}`}
-      aria-hidden={true}
-    />
-  </button>
-)
+interface MenuLineProps extends Pick<MenuButtonProps, 'isActive'> {
+  activeClass: string
+}
 
-const Menu = () => (
+const MenuPopup = () => (
   <div class='z-50 absolute bottom-[-128px] right-4 w-[200px] bg(gray-50 dark:[#1C1B1F]) py-5 px-4 rounded-lg shadow-md md:hidden'>
     <ul class='flex(& col) justify-center w-full gap-2'>
       {LINKS.map(({ path, label, ...item }) => (
@@ -43,4 +25,24 @@ const Menu = () => (
   </div>
 )
 
-export { Menu, MenuButton }
+const MenuButton = ({ toggleMenu, isActive }: MenuButtonProps) => (
+  <button
+    onClick={toggleMenu}
+    class='flex(& col) justify-between items-center w-7 h-[22px] md:hidden focus:outline-none'
+    aria-expanded={isActive}
+    aria-label={isActive ? 'Close nav menu' : 'Open nav menu'}
+  >
+    <MenuLine isActive={isActive} activeClass='rotate-45' />
+    <MenuLine isActive={isActive} activeClass='opacity-0' />
+    <MenuLine isActive={isActive} activeClass='-rotate-45' />
+  </button>
+)
+
+const MenuLine = ({ isActive, activeClass }: MenuLineProps) => (
+  <span
+    class={`${sharedMenuClasses} ${isActive ? activeClass : ''}`}
+    aria-hidden='true'
+  />
+)
+
+export { MenuButton, MenuPopup }
