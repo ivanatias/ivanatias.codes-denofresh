b9050755c350a3d014f2ab04b2555dbb6dbcdc01
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-23 15:13:35 -0400

    refactor(wrapper): add show header condition

--- components/layout/wrapper.tsx
+++ components/layout/wrapper.tsx
@@ -5,13 +5,14 @@ import Footer from 'components/layout/footer.tsx'
 
 interface Props {
   children: ComponentChildren
+  showHeader?: boolean
 }
 
-const Layout = ({ children }: Props) => (
+const Layout = ({ children, showHeader = true }: Props) => (
   <>
     <Navbar />
-    <main class='flex(& 1 col) max-w-3xl px-5 mx-auto'>
-      <Header />
+    <main class='flex(& 1 col) max-w-3xl px-5 mx-auto pt-32'>
+      {showHeader && <Header />}
       {children}
     </main>
     <Footer />
