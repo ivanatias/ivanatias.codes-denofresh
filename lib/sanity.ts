d345a1b7a5c573d350a6fba15af2ab90e7703244
Ivan Atias <ivan.d.atias@gmail.com>
2023-06-25 00:33:48 -0400

    🔧 refactor: add runtime check for sanity token

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,6 +1,12 @@
 import { type ClientConfig, createClient } from 'sanity/client'
 import imageUrlBuilder from 'sanity/image-url'
 
+const SANITY_TOKEN = Deno.env.get('SANITY_TOKEN')
+
+if (SANITY_TOKEN === undefined) {
+  throw new Error('SANITY_TOKEN must be set')
+}
+
 const config: ClientConfig = {
   projectId: 'uaj0umn5',
   dataset: 'production',
