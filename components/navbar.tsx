52a74cc68d114ba7779205416dc3fc5142ff710b
Ivan Atias <ivan.d.atias@gmail.com>
2023-07-08 18:32:28 -0400

    🔧 refactor: decrease navbar backdrop filter blur

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import Navlink from 'components/nav-link.tsx'
 import { LINKS } from 'constants/links.ts'
 
 const Navbar = () => (
-  <header class='fixed top-0 z-50 w-full py-5 bg-light-rgba dark:bg-dark-rgba backdrop-blur-md'>
+  <header class='fixed top-0 z-50 w-full py-5 bg-light-rgba dark:bg-dark-rgba backdrop-blur-sm'>
     <nav>
       <div class='flex items-center max-w-3xl mx-auto px-4 md:px-5'>
         <div class='flex flex-1 items-center gap-5'>
